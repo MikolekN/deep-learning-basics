da04d440aa68133d4d9a5df3f729bc004feac393
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-01-03 11:23:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from class_to_number import class_names
 from load_dataset import load_dataset
 from model import create_model, train_model, save_model, predict_image
 
-print("TensorFlow version:", tf.__version__)
 
 physical_devices = tf.config.list_physical_devices('GPU')
 if physical_devices:
@@ -15,13 +14,14 @@ else:
     print("No GPU detected. TensorFlow will run on CPU.")
 
 if physical_devices:
-    tf.config.set_visible_devices(physical_devices[0], 'GPU')  # Use the first GPU
+    tf.config.set_visible_devices(physical_devices[0], 'GPU')
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 train_ds, val_ds = load_dataset()
 model = create_model()
 history = train_model(model, train_ds, val_ds)
 save_model(model)
+
 prediction = predict_image(model, "data/testing/our/A-3/A-3.jpg")
 print(f"Prediction: {prediction}")
 predicted_class = np.argmax(prediction, axis=1)
